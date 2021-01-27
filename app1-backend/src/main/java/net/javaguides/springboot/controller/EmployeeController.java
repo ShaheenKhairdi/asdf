096fa71e64b41be762317d3ea0055aea8a468b0e
Redmine Admin <admin@example.net>
2021-01-27 21:04:36 +0530

    ini

--- app1-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ app1-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -21,7 +21,7 @@ import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
--- app1-backend/start_server.sh
+++ app1-backend/start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar -Dspring.profiles.active=prod /home/ec2-user/server/springboot-backend-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+java -jar -Dspring.profiles.active=prod /home/ec2-user/app1/springboot-backend-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
